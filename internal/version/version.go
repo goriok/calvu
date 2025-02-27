61706332e9980333e3a3b48dff91d2d3b5ff2678
igorsoares <igorsoares@ciandt.com>
2025-02-27 15:09:39 -0300

    feat: remove v prefix to avoid conflicts to strict semver contexts

--- internal/version/version.go
+++ internal/version/version.go
@@ -8,7 +8,7 @@ import (
 func FromTime(t time.Time) string{
 	y := t.Year() - 2000
 
-	return fmt.Sprintf("v%02d.%d.%02d%02d%02d", 
+	return fmt.Sprintf("%02d.%d.%02d%02d%02d", 
     y, 
     t.Month(), 
     t.Hour(), 
